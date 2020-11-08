0b9cccae17238e930ff08671484b60b707a2d5ef
Robert Ludwik <robertludwik96@gmail.com>
2020-11-08 16:11:14 -0500

    Loging Text change test

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  console.log("Working")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Pryvent</Text>
+      <TextInput placeholder="Username" />
+      <TextInput placeholder="Password" />
       <StatusBar style="auto" />
     </View>
   );
